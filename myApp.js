36955c35f54771163ff26518e3d71e71ca0e52d8
luthiena <171228113+luthiena@users.noreply.github.com>
2025-11-25 21:57:38 -0600

    modify

--- myApp.js
+++ myApp.js
@@ -1,7 +1,11 @@
-const express = require('express');
-const app = express();
+let express = require('express');
+let app = express();
 
 // Just log to the console
 console.log("Hello World");
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
 
 module.exports = app;
